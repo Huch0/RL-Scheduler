225fd2806a97ad928c17549e0ac6a48af065c18e
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-09-02 22:09:24 +0900

    feat : 초기 신경망 초기화에 xavier 추가

--- train/train_model.py
+++ train/train_model.py
@@ -13,6 +13,7 @@ from stable_baselines3.common.callbacks import CallbackList
 from stable_baselines3.common.monitor import Monitor
 
 from scheduler_env.customEnv_repeat import SchedulingEnv
+import torch.nn as nn
 import numpy as np
 
 class LinearStdDecayScheduler:
@@ -31,6 +32,25 @@ class LinearStdDecayScheduler:
     def step(self):
         # 스텝을 증가시키고 표준편차를 업데이트
         self.current_step += 1
+
+# class LogCostCallback(BaseCallback):
+#     def __init__(self, verbose=0):
+#         super(LogCostCallback, self).__init__(verbose)
+
+#     def _on_step(self) -> bool:
+#         # Get the info dictionary from the environment
+#         infos = self.locals['infos']  # This gives a list of info dicts from each environment step
+        
+#         # Assuming you have a single environment or care about the first env
+#         cost_tardiness = infos[0].get('cost_deadline', None)  # Extract the cost value
+#         cost_hole = infos[0].get('cost_hole', None)
+#         cost_makespan = infos[0].get('cost_makespan', None)
+#         cost_processing = infos[0].get('cost_processing', None)
+        
+#         # Log the cost value
+#         self.logger.record('train/cost', cost)
+        
+#         return True
 
 class UpdateStdCallback(BaseCallback):
     def __init__(self, std_scheduler, verbose=0):
@@ -51,6 +71,12 @@ class UpdateStdCallback(BaseCallback):
         
         return True
 
+# 모델 내부 파라미터를 무작위로 초기화
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
 
 def train_model(env, env_name, eval_env, params, version = "v1", total_steps = 1000000, algorithm = "MaskablePPO", deterministic = True):
     log_path = "./logs/tmp/" + env_name
@@ -75,11 +101,9 @@ def train_model(env, env_name, eval_env, params, version = "v1", total_steps = 1
     model.set_logger(new_logger)
 
     # Create the MaskableEvalCallback
-    # eval_env Monitor 감싸기
-    eval_env = Monitor(eval_env)
 
     maskable_eval_callback = MaskableEvalCallback(eval_env, best_model_save_path=log_path,
-                                                  log_path=log_path, eval_freq=50000,
+                                                  log_path=log_path, eval_freq=40960,
                                                   deterministic=deterministic, render=False)
     
     eval_callback = EvalCallback(eval_env, best_model_save_path=log_path, log_path=log_path, 
@@ -94,6 +118,7 @@ def train_model(env, env_name, eval_env, params, version = "v1", total_steps = 1
         callback = eval_callback
     # callback = CallbackList([maskable_eval_callback, update_std_callback])
 
+    model.policy.apply(init_weights)
     # Start the learning process
     model.learn(total_steps, callback=callback)
 
