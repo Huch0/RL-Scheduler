9da20ceca57a0c4b270ec793c78b9e5f0b923957
SeoJun <jun@SeoJunui-MacBookPro.local>
2024-09-02 15:01:53 +0900

    fix : final reward 구조 실수 고침

--- scheduler_env/customScheduler_repeat.py
+++ scheduler_env/customScheduler_repeat.py
@@ -664,33 +664,6 @@ class customRepeatableScheduler():
         mean_operation_duration_per_type = [np.mean(self.remain_op_duration_per_type[i]) if self.remain_op_duration_per_type[i] else 0 for i in range(self.num_of_types)]
         std_operation_duration_per_type = [np.std(self.remain_op_duration_per_type[i]) if self.remain_op_duration_per_type[i] else 0 for i in range(self.num_of_types)]
 
-        observation_v3 = {
-            # Operation Type별 지표
-            "total_count_per_type": np.array(total_count_per_type),
-            "mean_operation_duration_per_type": np.array(mean_operation_duration_per_type),
-            "std_operation_duration_per_type": np.array(std_operation_duration_per_type),
-            # 현 scheduling 상황 관련 지표
-            'last_finish_time_per_machine' : np.array([machine.cal_last_finish_time()//100 for machine in self.machines]),
-            'machine_ability' : np.array(machine_ability),
-            'hole_length_per_machine' : np.array(hole_length_per_machine),
-            "machine_utilization_rate": np.array(self.machine_operation_rate),
-            'remaining_repeats': np.array(remaining_repeats),
-            # schedule_buffer 관련 지표
-            'schedule_buffer_job_repeat': np.array(schedule_buffer_job_repeat),
-            'schedule_buffer_operation_index':  np.array(schedule_buffer_operation_index),
-            'cur_op_earliest_start': np.array(earliest_start_per_operation),
-            'cur_job_deadline': np.array(job_deadline),
-            'cur_op_duration': np.array(op_duration),
-            'cur_op_type': np.array(op_type),
-            "cur_remain_working_time" : np.array(remaining_working_time),
-            "cur_remain_num_op" : np.array(num_remaining_op),
-            # 추정 tardiness 관련 지표
-            'mean_estimated_tardiness_per_job': np.array(mean_estimated_tardiness_per_job),
-            'std_estimated_tardiness_per_job' : np.array(std_estimated_tardiness_per_job),
-            'cur_estimated_tardiness_per_job' : np.array(cur_estimated_tardiness_per_job),
-            # cost 관련 지표
-            'current_costs' : np.array([self.cost_deadline, self.cost_hole, self.cost_processing, self.cost_makespan])
-        }
         observation_v2 = {
             # Vaild 행동, Invalid 행동 관련 지표
             'action_masks': self.action_mask,
@@ -879,15 +852,14 @@ class customRepeatableScheduler():
         # return self.machine_term
 
     def calculate_final_reward(self):
-        # total_up_time = 0
-        # for i, repeat in enumerate(self.current_repeats):
-        #     total_duration_per_job = self.jobs[i][0].total_duration // 100
-        #     total_up_time += total_duration_per_job * repeat
+        total_up_time = 0
+        for i, repeat in enumerate(self.current_repeats):
+            total_duration_per_job = self.jobs[i][0].total_duration // 100
+            total_up_time += total_duration_per_job * repeat
 
-        # profit = total_up_time * self.profit_per_time
+        profit = total_up_time * self.profit_per_time
         cost = self.cal_final_cost()
-        return -1 * cost
-        # return ((profit - cost) / profit) * 100
+        return ((profit - cost) / profit) * 100
 
     def cal_job_deadline_cost(self):
         sum_of_time_exceed = 0
