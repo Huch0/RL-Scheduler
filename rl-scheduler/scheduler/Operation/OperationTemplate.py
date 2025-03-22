aaff16dd79f3b6768644e8f4b8ffadd6dfa8a546
sjin4861 <sjin4861@pusan.ac.kr>
2025-03-22 21:49:47 +0900

    fix : 스펙에 맞춰 멤버 변수에 predecesssor 추가

--- rl-scheduler/scheduler/Operation/OperationTemplate.py
+++ rl-scheduler/scheduler/Operation/OperationTemplate.py
@@ -8,8 +8,10 @@ class OperationTemplate:
         type_code: str,
         duration: int,
         job_template: JobTemplate,
+        predecessor: int, #이렇게 해도 None이 들어갈 수 있나?  검증 필요
     ):
         self.operation_template_id = operation_template_id
         self.type_code = type_code
         self.duration = duration
         self.job_template = job_template
+        self.predecessor = predecessor
