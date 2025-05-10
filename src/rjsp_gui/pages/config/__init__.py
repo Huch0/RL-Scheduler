351741e738382823462f4af3fa7db86191ef20f3
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-05-11 02:39:27 +0900

    chore : Training Configuration Tab 삭제

--- src/rjsp_gui/pages/config/__init__.py
+++ src/rjsp_gui/pages/config/__init__.py
@@ -1,7 +1,6 @@
 from .job import render_job_config
 from .machine import render_machine_config
 from .contract import render_contract_config
-from .training import render_training_config
 
 __all__ = ['render_job_config', 'render_machine_config', 
-          'render_contract_config', 'render_training_config']
+          'render_contract_config']
