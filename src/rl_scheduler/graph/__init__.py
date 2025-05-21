1a2643b6932272cb9c6cd17d02de0abfe2d20ce0
SeoJun <jun@SeoJunui-MacBookPro.local>
2025-05-21 14:04:56 +0900

    feat: add __init__.py to make graph module importable

--- src/rl_scheduler/graph/__init__.py
+++ src/rl_scheduler/graph/__init__.py
@@ -1,9 +1,9 @@
 from .sync import SchedulerGraphSync
-from .pyg_converter import graph_to_pyg
+from .pyg_converter import graph_to_heterodata
 from .builder import build_graph_from_scheduler
 
 __all__ = [
     "SchedulerGraphSync",
-    "graph_to_pyg",
+    "graph_to_heterodata",
     "build_graph_from_scheduler"
 ]
